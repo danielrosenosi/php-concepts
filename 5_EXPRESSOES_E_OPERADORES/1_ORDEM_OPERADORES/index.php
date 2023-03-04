dce6d1affec8c7c566e8abe17436a9a9d4d714d6
devroseno <devroseno@gmail.com>
2023-03-04 14:08:40 -0300

    feat: adding sub-section order of mathematical operators

---
+++ 5_EXPRESSOES_E_OPERADORES/1_ORDEM_OPERADORES/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo 3 + 2 * 5; // 13
+    echo "<br>";
+
+    echo (3 + 2) * 5; // 25
+    echo "<br>";
+
+    echo 5 + 2 / 10; // 5.2
+    echo "<br>";
+
+    echo 5 + 2 / (10 * 4); // 5.05
+    echo "<br>";
