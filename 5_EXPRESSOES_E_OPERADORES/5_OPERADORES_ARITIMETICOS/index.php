9ffcbde9e27c32f59854117a079b29af7287926b
devroseno <devroseno@gmail.com>
2023-03-04 14:39:57 -0300

    feat: adding subsection arithmetic operators

---
+++ 5_EXPRESSOES_E_OPERADORES/5_OPERADORES_ARITIMETICOS/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $valueOne = 10;
+    $valueTwo = 2;
+
+    echo "$valueOne + $valueTwo <br>"; // 12 (operador de adição)
+    echo "$valueOne - $valueTwo <br>"; // 8 (operador de subtração)
+    echo "$valueOne * $valueTwo <br>"; // 20 (operador de multiplicação)
+    echo "$valueOne / $valueTwo <br>"; // 5 (operador de divisão)
