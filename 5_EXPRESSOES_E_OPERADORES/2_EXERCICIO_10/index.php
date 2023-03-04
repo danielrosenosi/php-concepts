24083593b38320fc1e2626ec982b5dd72c8e1e4e
devroseno <devroseno@gmail.com>
2023-03-04 14:15:34 -0300

    feat: performing the tenth exercise

---
+++ 5_EXPRESSOES_E_OPERADORES/2_EXERCICIO_10/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $agePerson1 = 25;
+    $agePerson2 = 32;
+    $agePerson3 = 18;
+
+    $testingOperators = (($agePerson2 - $agePerson1) * $agePerson3) / 2;
+
+    echo $testingOperators;
+    echo "<br>";
+
+    // Fiz essa comparação apenas como bônus kkkkkkk
+    if($agePerson1 > $agePerson2) {
+        echo "A pessoa 1 é mais velha que a pessoa 2";
+    } else {
+        echo "A pessoa 2 é mais velha que a pessoa 1";
+    }
