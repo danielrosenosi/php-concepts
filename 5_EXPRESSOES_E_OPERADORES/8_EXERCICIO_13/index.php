31a7ca6f6f7b0f2467bbe09f3394e623b6f651f6
devroseno <devroseno@gmail.com>
2023-03-04 14:58:59 -0300

    feat: performing the thirteenth exercise

---
+++ 5_EXPRESSOES_E_OPERADORES/8_EXERCICIO_13/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $a = 11;
+    $b = 4;
+    $c = 2;
+
+    echo $a % $b; //Retorna 3, pois é o resto da divisão de 11 por 4
+    echo "<br>";
+    echo $b % $c; //Retorna 0, pois é o resto da divisão de 4 por 2
