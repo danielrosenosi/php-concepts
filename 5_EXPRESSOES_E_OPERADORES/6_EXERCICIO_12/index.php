a34a995e4fd11d06613e182363e5abf9083f3303
devroseno <devroseno@gmail.com>
2023-03-04 14:49:36 -0300

    feat: performing the twelve exercise

---
+++ 5_EXPRESSOES_E_OPERADORES/6_EXERCICIO_12/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $valueOne = 10;
+    $valueTwo = 2;
+
+    $sum = $valueOne + $valueTwo; //Soma
+    echo "Soma: $sum <br>";
+    
+    $valueThree = 5;
+
+    $subtracion = $sum - $valueThree; //Subtração
+    echo "Subtração: $subtracion <br>";
+
+    $valueFour = 2; 
+    
+    $multiplication = $subtracion * $valueFour; //Multiplicação
+    echo "Multiplicação: $multiplication <br>";
+
+    $valueFive = 22;
+
+    $division = $multiplication / $valueFive; //Divisão
+    echo "Divisão: $division <br>";
