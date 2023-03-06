28d55585f29e5594302a731f017471a0624b2cb9
devroseno <devroseno@gmail.com>
2023-03-06 18:47:21 -0300

    feat: adding subsection comparison operators

---
+++ 5_EXPRESSOES_E_OPERADORES/13_OPERADORES_DE_COMPARACAO/index.php
@@ -0,0 +1,27 @@
+<?php
+
+    $age = 17;
+
+    echo $age == 17; // 1 (TRUE)
+    echo "<br>";
+
+    echo $age === 17; // 1 (TRUE)
+    echo "<br>";
+
+    echo $age != 17; // 0 (FALSE)
+    echo "<br>";
+
+    echo $age !== 17; // 0 (FALSE)
+    echo "<br>";
+
+    echo $age > 17; // 0 (FALSE)
+    echo "<br>";
+
+    echo $age >= 17; // 1 (TRUE)
+    echo "<br>";
+
+    echo $age < 17; // 0 (FALSE)
+    echo "<br>";
+
+    echo $age <= 17; // 1 (TRUE)
+    echo "<br>";
