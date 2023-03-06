8037b7a6cbdfd627b25c089edc826aaf5962838f
devroseno <devroseno@gmail.com>
2023-03-06 19:06:42 -0300

    feat: adding subsection identical operator

---
+++ 5_EXPRESSOES_E_OPERADORES/15_OPERADOR_IDENTICO/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $numberOne = 1;
+    $stringOne = "1";
+
+    if($numberOne === $stringOne) {
+        echo "É IDÊNTICO";
+    } else {
+        echo "NÃO É IDÊNTICO"; 
+        // esse é o resultado da condição, pois o operador de identidade compara o valor e o tipo de dado
+    }
