b50d31bdce4358f178b81152ff43528e660e4dbe
devroseno <devroseno@gmail.com>
2023-03-06 19:02:01 -0300

    feat: performing the fifteenth exercise

---
+++ 5_EXPRESSOES_E_OPERADORES/14_EXERCICIO_15/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $numberOne = 1;
+    $numberTwo = 2;
+
+    if($numberOne == 1) {
+        echo "VERDADEIRO";
+    }
+
+    echo "<br>";
+
+    if($numberTwo == 1) {
+        echo "VERDADEIRO";
+    } else {
+        echo "FALSO";
+    }
