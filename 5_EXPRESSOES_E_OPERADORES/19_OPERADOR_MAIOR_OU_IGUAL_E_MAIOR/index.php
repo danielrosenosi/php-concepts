af7ddd36b540caa0a649a6e31bbfa7a3f1f2acce
devroseno <devroseno@gmail.com>
2023-03-07 20:51:53 -0300

    feat: adding subsection greater than or equal to

---
+++ 5_EXPRESSOES_E_OPERADORES/19_OPERADOR_MAIOR_OU_IGUAL_E_MAIOR/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $a = 10;
+    $b = 10;
+
+    if($a >= $b){
+        echo "A é maior ou igual a B";
+    } else {
+        echo "A não é maior ou igual a B";
+    }
+
+    echo "<br>";
+
+    if($a > $b){
+        echo "A é maior que B";
+    } else {
+        echo "A não é maior que B";
+    }
