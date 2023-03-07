9afb3eb41e0bf28de44a5516fb0af0370c5d8a77
devroseno <devroseno@gmail.com>
2023-03-07 20:57:06 -0300

    feat: adding subsection less than or equal to and less than

---
+++ 5_EXPRESSOES_E_OPERADORES/20_OPERADOR_MENOR_OU_IGUAL_E_MENOR/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $a = 10;
+    $b = 20;
+
+    if($a < $b) {
+        echo "A é menor que B";
+    } else {
+        echo "A é maior que B";
+    }
+
+    echo "<br>";
+
+    if($a <= $b) {
+        echo "A é menor ou igual a B";
+    } else {
+        echo "A não é menor ou igual a B";
+    }
