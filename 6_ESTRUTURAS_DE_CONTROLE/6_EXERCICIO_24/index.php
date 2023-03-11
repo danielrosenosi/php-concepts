c9f4cee822cd5e98871d84edbb438a7e0d5cba3e
devroseno <devroseno@gmail.com>
2023-03-11 15:46:48 -0300

    feat: performing twenty four exercise

---
+++ 6_ESTRUTURAS_DE_CONTROLE/6_EXERCICIO_24/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    $valueInteger = 1;
+    $valueFloat = 1.2;
+    $valueBoolean = true;
+    $valueString = "Daniel";
+
+    if(is_int($valueInteger)) {
+        echo "É um inteiro <br>";
+    } else {
+        echo "Não é um inteiro <br>";
+    }
+
+    if(is_int($valueFloat)) {
+        echo "É um inteiro <br>";
+    } else {
+        echo "Não é um inteiro <br>";
+    }
+
+    if(is_int($valueBoolean)) {
+        echo "É um inteiro <br>";
+    } else {
+        echo "Não é um inteiro <br>";
+    }
+
+    if(is_int($valueString)) {
+        echo "É um inteiro <br>";
+    } else {
+        echo "Não é um inteiro <br>";
+    }
