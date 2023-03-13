4f2007db836e6d1a2bff44ccc1ac5917522687b6
devroseno <devroseno@gmail.com>
2023-03-13 19:44:19 -0300

    feat: adding subsection if nested

---
+++ 6_ESTRUTURAS_DE_CONTROLE/8_IF_ANINHADO/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $age = 18;
+
+    if($age) {
+        if($age >= 18) {
+            echo "Maior de idade";
+        } else {
+            echo "Menor de idade";
+        }
+    } else {
+        echo "Idade não informada";
+    }
