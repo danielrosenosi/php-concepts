547b092e6537e883402dc45c43ad61301034f6c9
devroseno <devroseno@gmail.com>
2023-03-15 22:15:38 -0300

    feat: performing twenty seven exercise

---
+++ 7_ESTRUTURAS_DE_REPETIÇÃO/2_EXERCICIO_27/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $array = [1, 2, 3.1, true, [], 'teste', 'daniel', 3.14, 4, 5, 6, 7, 8, 9, 10];
+
+    while($array) {
+        $arrayValue = array_pop($array);
+
+        if(is_string($arrayValue)) {
+            echo $arrayValue . '<br>';
+        }
+    }
