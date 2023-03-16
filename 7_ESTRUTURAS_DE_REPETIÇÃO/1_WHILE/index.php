894a3388fa6e8bd0502db05c413662c1b6e9e798
devroseno <devroseno@gmail.com>
2023-03-15 22:10:11 -0300

    feat: adding subsection while

---
+++ 7_ESTRUTURAS_DE_REPETIÇÃO/1_WHILE/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $value = 0;
+
+    while($value < 10) { // Enquanto $value for menor que 10, ele vai executar o bloco de código abaixo
+        $value++; // Enquanto o $value for menor que 10, ele vai somar 1 ao $value
+    }
+
+    echo $value; // Ao fim da execução do while, o $value vai ser igual a 10
