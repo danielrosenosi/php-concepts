251f2b260b6175fff2c26be240702910b33ff7ce
devroseno <devroseno@gmail.com>
2023-03-15 21:39:49 -0300

    feat: adding subsection else if

---
+++ 6_ESTRUTURAS_DE_CONTROLE/10_ELSE_IF/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $age = 17;
+
+    if($age >= 18) {
+        echo "Você é maior de idade!";
+    } else if($age < 18) {
+        echo "Você é menor de idade1";
+    } else {
+        echo "Informe sua idade!";
+    }
