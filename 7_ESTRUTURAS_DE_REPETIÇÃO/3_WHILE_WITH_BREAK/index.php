ac89408ac12a18bd7233aec3045f6ac740db38f7
devroseno <devroseno@gmail.com>
2023-03-20 20:02:55 -0300

    feat: adding subsection while with break

---
+++ 7_ESTRUTURAS_DE_REPETIÇÃO/3_WHILE_WITH_BREAK/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $number = 1;
+
+    while($number) {
+        $number++;
+
+        if($number > 50) {
+            echo "$number é maior que 50 <br>"; // 51 é maior que 50
+            break;
+        }
+    }
