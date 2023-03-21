2f06d3c1beb0bb1430872716488c3d52b7b7818e
devroseno <devroseno@gmail.com>
2023-03-21 20:49:01 -0300

    feat: adding subsection while with continue

---
+++ 7_ESTRUTURAS_DE_REPETIÇÃO/6_CONTINUE/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $value = 10;
+
+    while($value > 0) {
+
+        if($value === 5 || $value === 5) {
+            echo "Pulou a execução $value" . "<br>";
+
+            $value--;
+            continue;
+        }
+
+        echo "Loop $value" . "<br>";
+
+        $value--;
+    }
