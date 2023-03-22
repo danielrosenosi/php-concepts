fc2f1c87abb90505dba0a2668a58769d30293879
devroseno <devroseno@gmail.com>
2023-03-22 19:29:33 -0300

    feat: adding subsection do while

---
+++ 7_ESTRUTURAS_DE_REPETIÇÃO/8_DO_WHILE/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $value = 0;
+
+    do {
+        echo $value . "<br>";
+        $value++;
+    } while($value <= 10);
+    
+    //Enquanto $value for menor ou igual a 10, ele vai executar o código que está dentro do (do)
