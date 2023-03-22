b37bfd5eaef652d52109443780fa1381ca2c68ac
devroseno <devroseno@gmail.com>
2023-03-22 19:34:23 -0300

    feat: adding subsection for

---
+++ 7_ESTRUTURAS_DE_REPETIÇÃO/9_FOR/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    //O for é o mais adequado para utilizar!
+
+    /*
+        for(CONTADOR; CONDIÇÃO; INCREMENTO/DESCREMENTO) {
+            CÓDIGO
+        }
+    */
+    
+    for($value = 0; $value <= 10; $value++) {
+        echo $value . "<br>";
+    }
