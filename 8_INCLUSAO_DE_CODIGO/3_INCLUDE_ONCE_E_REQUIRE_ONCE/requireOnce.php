fd0e4431c42918a0db8423990ede7ecf26cef9af
devroseno <devroseno@gmail.com>
2023-03-23 13:37:21 -0300

    feat: adding subsection include and require once

---
+++ 8_INCLUSAO_DE_CODIGO/3_INCLUDE_ONCE_E_REQUIRE_ONCE/requireOnce.php
@@ -0,0 +1,12 @@
+<?php
+
+    //Arquivos que não existem
+    //require_once ('arquivos/testando.php');
+
+    //Arquivos que existem
+    require_once ('arquivos/testeTwo.php');
+    require_once ('arquivos/testeTwo.php');
+
+    //O require_once só inclui o arquivo uma vez, mesmo que o comando seja chamado mais de uma vez
+?>
+    <p>Testando o require_once</p>
