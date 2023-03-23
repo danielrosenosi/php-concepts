4ce24031fc3b6819be8d773e2deee2f73b3e2f3f
danielrosenosi <carlos.daniel@multintegrada.com.br>
2023-03-23 11:30:16 -0300

    feat: adding subsection require

--- 8_INCLUSAO_DE_CODIGO/1_INCLUDE/index.php
+++ 8_INCLUSAO_DE_CODIGO/1_INCLUDE/index.php
@@ -1,6 +1,7 @@
 <?php
     //O include é utilizado para incluir um arquivo php em outro arquivo php
-    
+    //Se o arquivo não for encontrado, o include gera um warning e continua a execução do script
+
     //Ele traz tudo do arquivo teste.php para este arquivo, incluindo as variáveis
     include('teste.php');
 ?>
---
+++ 8_INCLUSAO_DE_CODIGO/2_REQUIRE/arquivos/teste2.php
@@ -0,0 +1,5 @@
+<h1>
+    Esse texto está no arquivo teste2.php, e teste2.php está incluído
+    em arquivoQueUtilizaOutroArquivo.php, e arquivoQueUtilizaOutroArquivo 
+    está incluído em encadeamentoDeArquivos.php
+</h1>
