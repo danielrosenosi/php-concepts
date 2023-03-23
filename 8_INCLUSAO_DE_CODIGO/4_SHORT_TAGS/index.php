4bfbd2ed4044208266dc58f281426873284531be
devroseno <devroseno@gmail.com>
2023-03-23 13:45:04 -0300

    feat: adding subsection short tags

---
+++ 8_INCLUSAO_DE_CODIGO/4_SHORT_TAGS/index.php
@@ -0,0 +1,6 @@
+NÃO É RECOMENDADO USAR SHORT TAGS, POIS PODEM CAUSAR PROBLEMAS
+DE COMPATIBILIDADE ENTRE OS SERVIDORES.	
+
+Exemplo de uso de Short Tags: <? echo "teste"; ?> 
+
+Esse código é equivalente a: <?php echo "teste"; ?>
