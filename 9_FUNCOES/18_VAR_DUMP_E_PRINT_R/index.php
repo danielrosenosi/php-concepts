3b614585c2907cd334f6b222fc873dae1fd4fffc
devroseno <devroseno@gmail.com>
2023-03-24 16:00:45 -0300

    feat: adding subsection var_dump and print_r for debug

---
+++ 9_FUNCOES/18_VAR_DUMP_E_PRINT_R/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $array = [
+        "Teste",
+        1,
+        1.2,
+        true
+    ];
+
+    //Debugando dados
+    print_r($array);
+    echo "<br>";
+    var_dump($array); //Esse é o recomendado a se usar, pois retorna informações mais detalhadas
