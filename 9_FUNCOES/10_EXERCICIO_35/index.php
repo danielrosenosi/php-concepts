d4f6d016d46195712e016b9abd44a84f05cc2f61
devroseno <devroseno@gmail.com>
2023-03-24 11:20:16 -0300

    feat: performing thirty five exercise

---
+++ 9_FUNCOES/10_EXERCICIO_35/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    //Função para calcular um número ao quadrado
+    function squareOfNumber($number) {
+        return $number * $number;
+    }
+
+    $value = 10;
+
+    echo squareOfNumber($value); //Vai imprimir 100
