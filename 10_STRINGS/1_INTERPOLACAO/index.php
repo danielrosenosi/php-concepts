d06f8acea63dcd55ffd240beffed663eb1a4de1c
devroseno <devroseno@gmail.com>
2023-03-26 16:04:38 -0300

    feat: adding subsection interpolation

---
+++ 10_STRINGS/1_INTERPOLACAO/index.php
@@ -0,0 +1,11 @@
+<?php
+    //OBSERVAÇÃO: MATENHA APENAS UM PADRÃO DE INTERPOLAÇÃO DURANTE TODO O PROJETO
+
+    $name = "Carlos Daniel";
+    $age = 17;
+
+    echo "Olá, eu me chamo $name e tenho $age anos"; //Primeira forma de interpolar uma variável
+
+    echo "<br>";
+
+    echo "Olá, eu me chamo {$name} e tenho {$age} anos"; //Segunda forma de interpolar uma variável
