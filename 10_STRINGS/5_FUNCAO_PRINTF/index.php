181e0b8e76655d05ea1b90c5eb1004be51374e29
devroseno <devroseno@gmail.com>
2023-03-26 16:43:23 -0300

    feat: adding subsection printf function

---
+++ 10_STRINGS/5_FUNCAO_PRINTF/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $name = "Carlos Daniel";
+    $numberInteger = 2;
+    $numberFloat = 12.4;
+
+    // %s => string
+    printf("O nome é %s <br>", $name);
+
+    // %d => int
+    printf("Um número é %d e outro é %d <br>", $numberInteger, 23);
+
+    // %f => float
+    printf("O a temperatura é %.2f", $numberFloat);
