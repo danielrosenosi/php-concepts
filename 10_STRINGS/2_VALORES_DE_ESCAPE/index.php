0eb103007a1b216d34cb637e73919b90bec87dc1
devroseno <devroseno@gmail.com>
2023-03-26 16:10:38 -0300

    feat: adding subsection escape values

---
+++ 10_STRINGS/2_VALORES_DE_ESCAPE/index.php
@@ -0,0 +1,14 @@
+<?php
+    header("Content-type: text/plain");
+
+    //pular linha
+    echo "Isso aqui vai ficar na primeira linha \n e isso aqui na segunda \n";
+
+    //tab
+    echo "Testando o tab \t aqui \n";
+
+    //barra invertida
+    echo "Barra invertida \\ \n";
+
+    //imprimindo o dólar
+    echo "Dólar \$";
