44eeadc7539c9879f958eeadfc172cd5ffe65575
devroseno <devroseno@gmail.com>
2023-03-27 08:58:45 -0300

    feat: adding subsection clean string

---
+++ 10_STRINGS/9_LIMPANDO_STRING/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $strOne = "  Carlos Daniel Roseno de Lima  ";
+    $strOneTrim = trim($strOne);
+    $strOneRtrim = trim($strOne);
+
+    echo "Essa é a primeira string, com espaço: $strOne." . "<br>";
+    echo "Essa é a primeira string, utilizando trim(): $strOneTrim." . "<br>";
+    echo "Essa é a primeira string, utilizando rtrim(): $strOneRtrim." . "<br>";
