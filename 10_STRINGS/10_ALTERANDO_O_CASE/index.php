878f8b9a3214c3f6697b680d97d0aa28952875ae
devroseno <devroseno@gmail.com>
2023-03-27 09:05:44 -0300

    feat: adding subsection changing the case

---
+++ 10_STRINGS/10_ALTERANDO_O_CASE/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$strOne = "string em caixa baixa";
+$strTwo = "STRING EM CAIXA BAIXA";
+
+echo strtoupper($strOne) . "<br>"; // Alterando o case para caixa alta
+echo strtolower($strTwo) . "<br>"; // Alterando o case para caixa baixa
