17f91d7f5ce6c82f427c0fde46b13ae2b3665060
devroseno <devroseno@gmail.com>
2023-03-29 13:44:31 -0300

    feat: adding subsection str_repeat

---
+++ 10_STRINGS/17_REPETINDO_STRINGS/index.php
@@ -0,0 +1,6 @@
+<?php
+    //str_repeat() repete uma string um número de vezes especificado pelo segundo parâmetro e retorna a string resultante
+    
+    $str = "Carlos Daniel ";
+
+    echo str_repeat($str, 5); // retorna "Carlos Daniel Carlos Daniel Carlos Daniel Carlos Daniel Carlos Daniel "
