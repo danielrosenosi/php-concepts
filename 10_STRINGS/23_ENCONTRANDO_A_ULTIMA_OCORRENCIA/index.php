be694bbd055bbbbc95ced9e6c39a5e4665059599
devroseno <devroseno@gmail.com>
2023-03-29 16:39:24 -0300

    feat: adding subsection strrpos

---
+++ 10_STRINGS/23_ENCONTRANDO_A_ULTIMA_OCORRENCIA/index.php
@@ -0,0 +1,7 @@
+<?php
+
+    $name = "Carlos Daniel Lima Roseno de Lima";
+
+    echo strrpos($name, "Lima") . "<br>"; //Retorna 29, pois é o indíce onde inicia a última ocorrência da palavra Lima
+
+    echo strpos($name, "Lima"); //Retorna 14, pois é o indíce onde inicia a primeira ocorrência da string "Lima" encontrada na string $name
