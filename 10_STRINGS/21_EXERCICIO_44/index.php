21ffc84a278d6d9ee72237b27ba3b73193078b26
devroseno <devroseno@gmail.com>
2023-03-29 16:14:57 -0300

    feat: performing forty four exercise

---
+++ 10_STRINGS/21_EXERCICIO_44/index.php
@@ -0,0 +1,7 @@
+<?php
+
+    $array = ["O", "PHP", "é", "muito", "legal"];
+
+    $transformingInString = implode(" ", $array);
+
+    echo $transformingInString; //retornou a string: O PHP é muito legal
