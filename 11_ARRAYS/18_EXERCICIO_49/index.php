6041d8ef4af675243ad91f4c72e999e3ed753d13
devroseno <devroseno@gmail.com>
2023-03-31 15:50:17 -0300

    feat: performing forty nine exercise

---
+++ 11_ARRAYS/18_EXERCICIO_49/index.php
@@ -0,0 +1,13 @@
+<?php
+    //CRIE VARIÁVEIS COM CARACTERÍSTICAS DE UM ANIMAL E CRIE UM ARRAY ASSOCIATIVO COM O COMPACT, COM TODAS ELAS
+    //APÓS ISSO, FAÇA UM LOOP E IMPRIMA OS VALORES DESSE ARRAY
+    $name = "Jack";
+    $color = "Marrom";
+    $race = "Labrador";
+    $age = 5;
+    
+    $featuresDog = compact('name', 'color', 'race', 'age');
+
+    foreach($featuresDog as $feature) {
+        echo $feature . "<br>";
+    }
