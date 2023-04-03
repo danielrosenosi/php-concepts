33afcf344ea1453b1ddbc0f2b0190f4475e85a55
devroseno <devroseno@gmail.com>
2023-04-03 10:13:39 -0300

    feat: adding subsection object-oriented introduction with PHP

---
+++ 12_INTRODUCAO_POO/1_CRIANDO_CLASS/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    //Inicial da class sempre em maiúsculo, exemplo: User
+    class User {
+        //Aqui, dentro da class, estarão as propriedades e métodos dessa class User
+    }
+
+    class Developer {
+        //Aqui, dentro da class, estarão as propriedades e métodos dessa class Developer
+    }
