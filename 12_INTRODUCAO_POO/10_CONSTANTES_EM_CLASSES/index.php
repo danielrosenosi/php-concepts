6bfe71d65206f5080df88caff2e2e723e74e5e83
danielrosenosi <devroseno@gmail.com>
2023-04-20 08:34:08 -0300

    feat: finishing section constants in classes

--- 12_INTRODUCAO_POO/10_CONSTANTES_EM_CLASSES/index.php
+++ 12_INTRODUCAO_POO/10_CONSTANTES_EM_CLASSES/index.php
@@ -1,3 +1,20 @@
 <?php
 
-    
+    class Human {
+        public const EYES = 2;
+        public const ARMS = 2;
+        public const LEGS = 2;
+
+        //ACESSAR CONST EM UM MÉTODO
+        function showConst() {
+            echo self::EYES . " olhos (acessando no método)" . "<br>";
+        }
+    }
+
+    $carlos = new Human;
+
+    echo $carlos::EYES . " olhos" . "<br>";
+    echo $carlos::ARMS . " braços" . "<br>";
+    echo $carlos::LEGS . " pernas" . "<br>";
+
+    echo $carlos->showConst();
