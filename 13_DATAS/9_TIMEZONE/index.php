8ccd9835635002d19b9318a34ea1a0ccb388dc66
danielrosenosi <devroseno@gmail.com>
2023-04-24 11:14:09 -0300

    feat: adding subsection timezone

---
+++ 13_DATAS/9_TIMEZONE/index.php
@@ -0,0 +1,7 @@
+<?php
+
+    date_default_timezone_set('America/Sao_Paulo');
+
+    $data = new DateTime();
+
+    print_r($data); //DateTime Object ( [date] => 2023-04-24 11:12:55.365837 [timezone_type] => 3 [timezone] => America/Sao_Paulo )
