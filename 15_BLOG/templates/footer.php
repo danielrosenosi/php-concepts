89726db47367ccaa144a96352a2f4323fe778cc9
devroseno <devroseno@gmail.com>
2023-07-16 11:32:08 -0300

    feat: blog in progress

---
+++ 15_BLOG/templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Dev Roseno &copy; 2023</p>
+</footer>
+</body>
+</html>
---
+++ 15_BLOG/templates/header.php
@@ -0,0 +1,41 @@
+<?php
+    include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Roseno</title>
+
+    <!-- CSS STYLES -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>assets/css/styles.css">
+
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a href="<?php $BASE_URL ?>" id="logo">
+            <img src="<?php $BASE_URL ?>assets/images/logo.svg" alt="Dev Roseno">
+        </a>
+
+        <nav>
+            <ul id="navbar">
+                <li>
+                    <a href="<?php $BASE_URL ?>index.php" class="nav-link">Início</a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link">Categorias</a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link">Sobre</a>
+                </li>
+                <li>
+                    <a href="<?php $BASE_URL ?>contact.php" class="nav-link">Contato</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
