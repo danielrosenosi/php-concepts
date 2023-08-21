58c74bff85656c3aca85e8d553ecbe6d7ecfed23
danielrosenosi <devroseno@gmail.com>
2023-08-21 11:03:22 -0300

    style: creating and styling the home

--- 15_BLOG/index.php
+++ 15_BLOG/index.php
@@ -1,7 +1,37 @@
 <?php
-    include_once("templates/header.php");
+    global $posts, $BASE_URL;
+
+    include_once ("templates/header.php");
 ?>
-    <h1>Olá mundo!</h1>
+    <main>
+        <div id="title_container">
+            <h1>Blog Dev Roseno</h1>
+
+            <p>O seu blog de progamação</p>
+        </div>
+
+        <div id="posts_container">
+            <?php foreach ($posts as $post): ?>
+                <div class="post-box">
+                    <img src="<?= $BASE_URL ?>/assets/images/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+
+                    <h2 class="post-title">
+                        <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
+                    </h2>
+
+                    <p class="post-description">
+                        <?= $post['description'] ?>
+                    </p>
+
+                    <div class="tags-container">
+                        <?php foreach ($post['tags'] as $tag): ?>
+                            <a href="#"><?= $tag ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </main>
 <?php
     include_once("templates/footer.php");
 ?>
